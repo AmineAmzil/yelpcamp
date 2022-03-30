dc83c7d21e14197995e05b4440b22b9a286f7a25
Amine Amzil <75944612+AmineAmzil@users.noreply.github.com>
2022-03-30 21:23:52 +0200

    client side and server side validation

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const { campgroundSchema } = require("./schemas");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 
 const Campground = require("./models/campground");
 
@@ -26,6 +29,17 @@ app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(", ");
+    console.log(msg);
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -34,45 +48,74 @@ app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
 
-app.get("/campgrounds/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
-  res.render("campgrounds/edit", { campground });
+app.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/edit", { campground });
+  }),
+);
+
+app.put(
+  "/campgrounds/:id",
+  validateCampground,
+  catchAsync(async (req, res) => {
+    const { campground } = req.body;
+    const { id } = req.params;
+
+    await Campground.findByIdAndUpdate(id, { ...campground }, { new: true });
+
+    res.redirect(`/campgrounds/${id}`);
+  }),
+);
+
+app.delete(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+
+    await Campground.findByIdAndDelete(id);
+
+    res.redirect(`/campgrounds`);
+  }),
+);
+
+app.get(
+  "/campgrounds",
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find();
+    res.render("campgrounds/index", { campgrounds });
+  }),
+);
+
+app.post(
+  "/campgrounds",
+  catchAsync(async (req, res) => {
+    const { campground } = req.body;
+    const camp = new Campground(campground);
+    await camp.save();
+    res.redirect(`/campgrounds/${camp._id}`);
+  }),
+);
+
+app.get(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/show", { campground });
+  }),
+);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
 });
 
-app.put("/campgrounds/:id", async (req, res) => {
-  const { campground } = req.body;
-  const { id } = req.params;
-
-  await Campground.findByIdAndUpdate(id, { ...campground }, { new: true });
-
-  res.redirect(`/campgrounds/${id}`);
-});
-
-app.delete("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-
-  await Campground.findByIdAndDelete(id);
-
-  res.redirect(`/campgrounds`);
-});
-
-app.get("/campgrounds", async (req, res) => {
-  const campgrounds = await Campground.find();
-  res.render("campgrounds/index", { campgrounds });
-});
-
-app.post("/campgrounds", async (req, res) => {
-  const { campground } = req.body;
-  const camp = new Campground(campground);
-  await camp.save();
-  res.redirect(`/campgrounds/${camp._id}`);
-});
-
-app.get("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
-  res.render("campgrounds/show", { campground });
+app.use((error, req, res, next) => {
+  const { statusCode = 500 } = error;
+  if (!error.message) error.message = "Oh no somthing went wrong";
+  res.status(statusCode).render("error", { error });
 });
 
 app.listen(PORT, () => {
---
+++ utils/catchAsync.js
@@ -0,0 +1,5 @@
+module.exports = function (fun) {
+  return (req, res, next) => {
+    fun(req, res, next).catch((e) => next(e));
+  };
+};
