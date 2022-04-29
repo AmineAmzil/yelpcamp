041c1ccbfd3717a6d6d3c467a860425d07147af7
Amine Amzil <75944612+AmineAmzil@users.noreply.github.com>
2022-04-29 17:57:26 +0200

    added user model,  registration and login forms

--- app.js
+++ app.js
@@ -11,10 +11,14 @@ const Review = require("./models/review");
 const Campground = require("./models/campground");
 const campgroundsRouter = require("./routes/campgroundsRouter");
 const reviewsRouter = require("./routes/reviewsRouter");
+const userRouter = require("./routes/usersRoutes");
 const PORT = process.env.PORT;
 const session = require("express-session");
 const flash = require("connect-flash");
 const app = express();
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 mongoose.connect(process.env.DBLINK);
 const db = mongoose.connection;
@@ -25,6 +29,7 @@ db.once("open", () => {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 
 const sessionConfig = {
   secret: "bettersecret key should be here",
@@ -37,15 +42,24 @@ const sessionConfig = {
   },
 };
 
-app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(flash());
 app.use(session(sessionConfig));
 
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 // Routes
+
+app.get("/fakeuser", async (req, res) => {
+  const user = new User({ email: "fakeuser@gmail.com", username: "fakeuser" });
+  const newUser = await User.register(user, "fakepassword");
+  res.json(newUser);
+});
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
@@ -53,6 +67,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/", userRouter);
 app.use("/campgrounds", campgroundsRouter);
 app.use("/campgrounds/:id/reviews", reviewsRouter);
 
